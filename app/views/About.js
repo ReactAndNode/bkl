cb670391d6d22cacf02b8ceb08a6975244acdd0c
Luca <bowenkliu@gmail.com>
2019-03-24 16:41:12 -0400

    getting the picture gallery 1.0

--- app/views/About.js
+++ app/views/About.js
@@ -1,8 +1,62 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Helmet from 'react-helmet';
+import Gallery from "react-photo-gallery";
 
 import Main from '../layouts/Main';
+
+
+const photos = [
+    {
+        src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+        width: 1,
+        height: 1
+    },
+    {
+        src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+        width: 4,
+        height: 3
+    },
+    {
+        src: "https://source.unsplash.com/zh7GEuORbUw/",
+        width: 3,
+        height: 4
+    },
+    {
+        src: "https://source.unsplash.com/PpOHJezOalU/",
+        width: 4,
+        height: 3
+    }
+];
+
+function columns(containerWidth) {
+    let columns = 1;
+    if (containerWidth >= 500) columns = 2;
+    if (containerWidth >= 900) columns = 3;
+    if (containerWidth >= 1500) columns = 4;
+    return columns;
+}
+
 
 const About = () => (
     <Main>
@@ -10,11 +64,11 @@ const About = () => (
         <article className="post" id="about">
             <header>
                 <div className="title">
-                    <h2><Link to="/about"> pic a boo</Link></h2>
+                    <h2><Link to="/about"> picture </Link></h2>
                     <p> Not the best photographer but I do enjoy pictures :)</p>
                 </div>
             </header>
-            <p> Insert pictures here</p>
+            <Gallery photos={photos} columns={columns}/>
         </article>
     </Main>
 );
